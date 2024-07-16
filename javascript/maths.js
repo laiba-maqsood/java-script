fdbcaaebd5221aeee5095331758cb1fd65cc34ef
Laiba Maqsood <143703759+laiba-maqsood@users.noreply.github.com>
2024-07-16 09:45:08 +0000

    maths javascript done

---
+++ javascript/maths.js
@@ -0,0 +1,7 @@
+let number = 10.876
+// console.log(Math.round(number));
+// console.log(Math.random());
+// console.log(Math.round((Math.random()*10)+1));
+const min= 5
+const max= 19
+console.log(Math.round(((Math.random()*(max-min )))+min));
